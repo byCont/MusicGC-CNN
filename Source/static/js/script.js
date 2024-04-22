63fc2d026c029c1e050a5f1442af6825d426c1d5
dicontrerasb <dicontrerasb@unal.edu.co>
2024-04-21 20:53:08 -0500

    "menores"

---
+++ Source/static/js/script.js
@@ -0,0 +1,19 @@
+type =
+  "text/javascript" >
+  barba.init({
+    transitions: [
+      {
+        name: "opacity-transition",
+        leave(data) {
+          return gsap.to(data.current.container, {
+            opacity: 0,
+          });
+        },
+        enter(data) {
+          return gsap.from(data.next.container, {
+            opacity: 0,
+          });
+        },
+      },
+    ],
+  });
